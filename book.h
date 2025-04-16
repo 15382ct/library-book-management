bb380e87a90f1187a760c9cab657717dde4782c9
15382ct <15382@ait.nsw.edu.au>
2025-04-16 11:31:07 +1000

    book.h

--- book.h
+++ book.h
@@ -1,1 +1,59 @@
+// Book.h
+#ifndef BOOK_H
+#define BOOK_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Book {
+protected:
+    string title;
+    string author;
+    string isbn;
+    bool available;
+
+public:
+    Book() {}
+
+    Book(string t, string a, string i, bool avail = true) {
+        title = t;
+        author = a;
+        isbn = i;
+        available = avail;
+    }
+
+    virtual void setBookDetails(string t, string a, string i, bool avail = true) {
+        title = t;
+        author = a;
+        isbn = i;
+        available = avail;
+    }
+
+    virtual void displayBookDetails() const {
+        cout << "\nTitle: " << title
+             << "\nAuthor: " << author
+             << "\nISBN: " << isbn
+             << "\nAvailability: " << (available ? "Available" : "Borrowed") << endl;
+    }
+
+    string getISBN() const {
+        return isbn;
+    }
+
+    bool isAvailable() const {
+        return available;
+    }
+
+    void borrowBook() {
+        if (available) {
+            available = false;
+            cout << "You have successfully borrowed \"" << title << "\"." << endl;
+        } else {
+            cout << "Sorry, \"" << title << "\" is already borrowed." << endl;
+        }
+    }
+
+    void returnBook() {
+        available
 
