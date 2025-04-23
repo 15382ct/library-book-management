2885351fb0a147615ff0407f6bc077ddd5c837a7
15382ct <15382@ait.nsw.edu.au>
2025-04-24 09:08:37 +1000

    Update book.cpp

--- book.cpp
+++ book.cpp
@@ -1,1 +1,4 @@
+Book book1("The Alchemist", "Paulo Coelho", "9780061122415", true, "2023-01-12");
+Book book2("1984", "George Orwell", "9780451524935", false, "2022-11-20");
+Book book3("To Kill a Mockingbird", "Harper Lee", "9780060935467", true, "2021-05-18");
 
